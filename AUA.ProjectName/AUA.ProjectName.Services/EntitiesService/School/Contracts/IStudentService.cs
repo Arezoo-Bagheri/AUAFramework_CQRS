b64d628b7536f7d22a46a349c6587f14f5ebaa46
Arezo Bagheri <ArezoBagheri@outlook.com>
2024-05-22 12:39:09 +0330

    Add : StudentService

---
+++ AUA.ProjectName/AUA.ProjectName.Services/EntitiesService/School/Contracts/IStudentService.cs
@@ -0,0 +1,9 @@
+﻿using AUA.Infrastructure.BaseServices;
+using AUA.ProjectName.DomainEntities.Entities.School;
+
+namespace AUA.ProjectName.Services.EntitiesService.School.Contracts
+{
+    public interface IStudentService : IDomainEntityService<Student>
+    {
+    }
+}
---
+++ AUA.ProjectName/AUA.ProjectName.Services/EntitiesService/School/Services/StudentService.cs
@@ -0,0 +1,10 @@
+﻿using AUA.Infrastructure.BaseServices;
+using AUA.ProjectName.DomainEntities.Entities.School;
+using AUA.ProjectName.Services.EntitiesService.School.Contracts;
+
+namespace AUA.ProjectName.Services.EntitiesService.School.Services
+{
+    public class StudentService : DomainEntityService<Student>, IStudentService
+    {
+    }
+}
